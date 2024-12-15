692ab865a3f1aff92e92cdb642cd4adf6ef22e5c
ismailnjb <mohammed.ismail.njb@gmail.com>
2024-12-15 08:47:48 +0530

    n

--- browser.py
+++ browser.py
@@ -7,34 +7,33 @@ class Browser(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # Set up the UI components
-        self.browser = QWebEngineView()  # Web Engine view to render pages
-        self.browser.setUrl(QUrl("http://www.google.com"))  # Default URL
+
+        self.browser = QWebEngineView()  
+        self.browser.setUrl(QUrl("http://www.google.com"))  
         self.setCentralWidget(self.browser)
 
         self.setWindowTitle("Simple Browser")
         self.setGeometry(100, 100, 1024, 768)
 
-        # Navigation bar
+       
         navbar = QToolBar()
         self.addToolBar(navbar)
 
-        # Back button
+   
         back_btn = QAction('Back', self)
         back_btn.triggered.connect(self.browser.back)
         navbar.addAction(back_btn)
 
-        # Forward button
+      
         forward_btn = QAction('Forward', self)
         forward_btn.triggered.connect(self.browser.forward)
         navbar.addAction(forward_btn)
 
-        # Reload button
+      
         reload_btn = QAction('Reload', self)
         reload_btn.triggered.connect(self.browser.reload)
         navbar.addAction(reload_btn)
 
-        # Address bar
         self.url_bar = QLineEdit()
         self.url_bar.returnPressed.connect(self.navigate_to_url)
         navbar.addWidget(self.url_bar)
@@ -48,7 +47,6 @@ class Browser(QMainWindow):
     def update_url(self, q):
         self.url_bar.setText(q.toString())
 
-# Initialize the application and the main window
 app = QApplication(sys.argv)
 QApplication.setApplicationName("Simple Browser")
 
